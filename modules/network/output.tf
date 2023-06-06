9c01ced8370fa88292a0e3b7937cc3304b1c9607
Jake Mallory <jake+git@tinomen.com>
2023-06-06 08:20:50 -0600

    add output resources

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -16,3 +16,27 @@ output "bastion_host_ip" {
 output "bastion_host_dns" {
   value = aws_instance.admin_bastion.public_dns
 }
+output "load_balancer_ip" {
+  value = aws_lb.wp.dns_name
+}
+output "db_security_group_id" {
+  value = aws_security_group.db.id
+}
+output "efs_file_system_id" {
+  value = aws_efs_file_system.wordpress.id
+}
+output "efs_access_point_id" {
+  value = aws_efs_access_point.wordpress_root.id
+}
+output "ecs_security_group_id" {
+  value = aws_security_group.ecs_service.id
+}
+output "http_target_group_arn" {
+  value = aws_lb_target_group.wp.arn
+}
+output "cloudwatch_log_name" {
+  value = aws_cloudwatch_log_group.wordpress.name
+}
+output "ecs_task_execution_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
